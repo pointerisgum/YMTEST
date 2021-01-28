ec874087bcb5a6ccda117f6202bbdd7b498485c5
김영민 <pointerisgum@gmail.com>
2021-01-28 16:25:26 +0900

    Initial Commit

---
+++ YMTEST/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  YMTEST
+//
+//  Created by 김영민 on 2020/03/05.
+//  Copyright © 2020 김영민. All rights reserved.
+//
+
+//SubMaster3
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ YMTEST/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YMTEST
+//
+//  Created by 김영민 on 2020/03/05.
+//  Copyright © 2020 김영민. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
